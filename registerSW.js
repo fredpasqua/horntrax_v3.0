1c0ec40a73f16319d9ca412203b7d085a084add8
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2026-02-10 23:09:24 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/horntrax_v3.0/sw.js', { scope: '/horntrax_v3.0/' })})}
